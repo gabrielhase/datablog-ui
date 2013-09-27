b39fc345f94261edc44f3e5406d6c2d5f3fe5b95
Gabriel <gabriel.hase@gmail.com>
2013-09-27 12:05:21 +0200

    add mocha/chai/sinon for unit tests
    - rewrites karma.conf
    - test are now executed with karma start and karma
    run

--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,82 @@
 // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-  // the big guns
-  'app/vendor/jquery-1.9.1.js',
-  'app/vendor/angular/angular.js',
-  'app/vendor/angular/angular-mocks.js',
 
-  // livingdocs designs
-  'app/vendor/bootstrap-design/bootstrap.js',
+    // frameworks to use
+    frameworks: ['mocha', 'sinon-chai'],
 
-  // livingdocs-engine
-  "app/vendor/livingdocs-engine/vendor/yepnope/yepnope.1.5.3-min.js",
-  "app/vendor/livingdocs-engine/vendor/store/store.js",
-  "app/vendor/livingdocs-engine/vendor/editableJS/editable.js",
-  "app/vendor/livingdocs-engine/livingdocs_engine.js",
 
-  // specs
-  '.tmp/editor_test.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
 
-// list of files to exclude
-exclude = [];
+      // the big guns
+      'app/vendor/jquery-1.9.1.js',
+      'app/vendor/angular/angular.js',
+      'app/vendor/angular/angular-mocks.js',
+      'app/vendor/angular-ui-bootstrap/*.js',
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+      // livingdocs designs
+      'app/vendor/livingmaps-design/design.js',
 
-// web server port
-port = 8080;
+      // livingdocs-engine
+      'app/vendor/livingdocs-engine/vendor/yepnope/yepnope.1.5.3-min.js',
+      'app/vendor/livingdocs-engine/vendor/store/store.js',
+      'app/vendor/livingdocs-engine/vendor/editableJS/editable.js',
+      'app/vendor/livingdocs-engine/livingdocs_engine.js',
 
-// cli runner port
-runnerPort = 9100;
+      // Maps
+      'app/vendor/leaflet/angular_leaflet_directive.js',
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+      // API
+      'app/scripts/components/environment/constants.js',
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+      // specs
+      '.tmp/editor_test.js'
+    ],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // list of files to exclude
+    exclude: [],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome', 'Firefox', 'Safari'];
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['progress'],
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // web server port
+    port: 8080,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // cli runner port
+    runnerPort: 9100,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+  });
+};
