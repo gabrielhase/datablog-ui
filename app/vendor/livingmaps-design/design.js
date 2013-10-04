2a6577f0d1a54d47ec30e6fa7827ff2e675365a9
Gabriel <gabriel.hase@gmail.com>
2013-10-04 13:39:38 +0200

    add choropleth snippet

--- app/vendor/livingmaps-design/design.js
+++ app/vendor/livingmaps-design/design.js
@@ -13,6 +13,13 @@
           "mainAndSidebar"
         ]
       },
+      "maps": {
+        "title": "maps",
+        "templates": [
+          "choropleth",
+          "map"
+        ]
+      },
       "others": {
         "title": "others",
         "templates": [
@@ -21,7 +28,6 @@
           "image",
           "info",
           "largeButton",
-          "map",
           "seperator",
           "smallSubtitle",
           "subtitle",
@@ -41,6 +47,16 @@
       "title": "Main and Sidebar Columns",
       "id": "mainAndSidebar",
       "html": "<div class=\"row-fluid\"><div class=\"span8\" doc-container=\"main\"></div><div class=\"span4\" doc-container=\"sidebar\"></div></div>"
+    },
+    {
+      "title": "Choropleth",
+      "id": "choropleth",
+      "html": "<div data-template=\"\"><h3 doc-editable=\"title\">Title</h3><div data-is=\"d3-choropleth\" data-dependency=\"d3\" data-dependency-resources=\"http://d3js.org/d3.v3.min.js\"><p>Couldn't load d3.js dependency. Maybe the service is down?</p></div></div>"
+    },
+    {
+      "title": "Map",
+      "id": "map",
+      "html": "<div data-template=\"\"><h3 doc-editable=\"title\">Title</h3><div data-is=\"leaflet-map\" data-dependency=\"L\" data-dependency-resources=\"http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js;http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css\"><iframe style=\"width: 100%\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"https://maps.google.ch/maps?hl=de&amp;ie=UTF8&amp;ll=46.362093,9.036255&amp;spn=8.279854,10.612793&amp;t=h&amp;z=7&amp;output=embed\"></iframe></div></div>"
     },
     {
       "title": "Button",
@@ -66,11 +82,6 @@
       "title": "Large Button",
       "id": "largeButton",
       "html": "<div><hr></div>"
-    },
-    {
-      "title": "Map",
-      "id": "map",
-      "html": "<div data-template=\"\"><h3 doc-editable=\"title\">Title</h3><div data-is=\"leaflet-map\" data-dependency=\"L\" data-dependency-resources=\"http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js;http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css\"><iframe style=\"width: 100%\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"https://maps.google.ch/maps?hl=de&amp;ie=UTF8&amp;ll=46.362093,9.036255&amp;spn=8.279854,10.612793&amp;t=h&amp;z=7&amp;output=embed\"></iframe></div></div>"
     },
     {
       "title": "Seperator",
